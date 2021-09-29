4c8a35dc46f594e1e3d51bd81127037677f0f4d5
mark <mareczektoja@protonmail.com>
2021-09-29 18:27:11 +0200

    first commit

---
+++ prostokat.js
@@ -0,0 +1,14 @@
+ax=1
+ay=3
+bx=1
+by=1
+cx=5
+cy=3
+dx=5
+dy=1
+
+if (ay==cy&&by==dy&&ax==bx&&cx==dx) {
+    console.log("Można stworzyć prostokąt")
+} else {
+    console.log("nie można stworzyć prostokątu")
+}
